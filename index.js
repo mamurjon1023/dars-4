baf11c29823970276bd44e2d4c1117edd1897427
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-01 14:02:29 +0500

    done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let son = Number(prompt("Sonni kiriting"));
 
-if ( son % 3 === 0 && son % 5 === 0) {
-    alert("Fizzbuzz")
+if (son % 3 === 0 && son % 5 === 0) {
+    alert(`${son} - Fizzbuzz`)
 }
 else if (son % 3 === 0) {
-    alert("Fizz");
+    alert(`${son} - Fizz`)
 }
 else if (son % 5 === 0) {
     alert("Buzz")
 }
-else if (son % 15 === 0) {
-    alert("Fizzbuzz")
+else {
+
 }
